7e8015a431468758fdddf93220adfffae7767093
Jaga <jagadishvaranasi123@gmail.com>
2020-05-02 14:06:20 +0530

    added struct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,8 +2,10 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
   uint storedData;
-  address owner;
+  struct request{
 
+  }
+  address owner;
   function set(uint x) public {
     storedData = x;
   }
