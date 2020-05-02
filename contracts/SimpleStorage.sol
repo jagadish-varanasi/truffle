017262018d2979d807b825ca1cd3ceed528f0060
Jaga <jagadishvaranasi123@gmail.com>
2020-05-02 13:52:28 +0530

    add basic templet

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract SimpleStorage {
+  uint storedData;
+  address owner;
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+}
